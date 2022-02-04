d6bbc9e6593d3eb7295b12e0c091fdca9b88f0e4
Elaheh Ahmadi <elahea@github.mit.edu>
2022-02-04 01:11:17 -0500

    updated the demo

--- audit_demo/js/debias_training_report.js
+++ audit_demo/js/debias_training_report.js
@@ -36,17 +36,74 @@ function label_metrics()
 if(document.images)
   {
 	  idx=document.label_metrics_form.metric.options[document.label_metrics_form.metric.selectedIndex].text.charAt(0)
-	  document.label_metrics_name.data = "demo_material/model_comparison_metrics_label_"+idx+".html"
+	  document.label_metrics_name.data = "demo_material/debias_training/model_comparison_metrics_label_"+idx+".html"
 	  console.log(document.label_metrics_name.data)
   }
 }
 
-function advanced_metrics()
+function advanced_metrics_baseline_model()
 {
-  idx=document.advanced_metrics_form.metric.options[document.advanced_metrics_form.metric.selectedIndex].text
+	idx=document.baseline_model_advanced_metrics_form.metric.options[document.baseline_model_advanced_metrics_form.metric.selectedIndex].text
+	console.log(idx)
+
   if(idx=="train loss")
   {
-	  document.advanced_metrics_name.data = "demo_material/Training_and_Accuracy_Metrics_for_uniform_on_histories.html"
+	  document.baseline_model_advanced_metrics_name.data = "demo_material/debias_training/uniform_model_history_plots.html"
   }
+	else if(idx=="label accuracy")
+  {
+	  document.baseline_model_advanced_metrics_name.data = "demo_material/debias_training/uniform_model_label_acc_row.html"
+  }
+	else if(idx=="meta accuracy per cluser")
+  {
+	  document.baseline_model_advanced_metrics_name.data = "demo_material/debias_training/uniform_model_meta_accs_per_cluster_row.html"
+  }
+	else if(idx=="meta accuracy")
+	{
+		document.baseline_model_advanced_metrics_name.data = "demo_material/debias_training/uniform_model_meta_accs_plot.html"
+	}
 }
+
+function advanced_metrics_debiased_model()
+{
+	idx=document.debiased_model_advanced_metrics_form.metric.options[document.debiased_model_advanced_metrics_form.metric.selectedIndex].text
+  if(idx=="train loss")
+  {
+	  document.debiased_model_advanced_metrics_name.data = "demo_material/debias_training/debiased_model_history_plots.html"
+  }
+	else if(idx=="label accuracy")
+  {
+	  document.debiased_model_advanced_metrics_name.data = "demo_material/debias_training/debiased_model_label_acc_row.html"
+  }
+	else if(idx=="meta accuracy per cluser")
+  {
+	  document.debiased_model_advanced_metrics_name.data = "demo_material/debias_training/debiased_model_meta_accs_per_cluster_row.html"
+  }
+	else if(idx=="meta accuracy")
+	{
+		document.debiased_model_advanced_metrics_name.data = "demo_material/debias_training/debiased_model_meta_accs_plot.html"
+	}
+}
+
+function training_history()
+{
+	idx=document.training_history_form.metric.options[document.training_history_form.metric.selectedIndex].text
+  if(idx=="train weighted loss")
+  {
+	  document.training_history_name.data = "demo_material/debias_training/training_history_comparison_plot_train__weighted_loss.html"
+  }
+	else if(idx=="train prediction loss")
+  {
+	  document.training_history_name.data = "demo_material/debias_training/training_history_comparison_plot_train__prediction_loss.html"
+  }
+	else if(idx=="test weighted loss")
+  {
+	  document.training_history_name.data = "demo_material/debias_training/training_history_comparison_plot_test__weighted_loss.html"
+  }
+	else if(idx=="test prediction loss")
+	{
+		document.training_history_name.data = "demo_material/debias_training/training_history_comparison_plot_test__prediction_loss.html"
+	}
+}
+
 window.onerror = null;
